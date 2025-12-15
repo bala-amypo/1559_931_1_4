5e7dc763a7acdad38d9db20fbc32016d722329bd
bala-amypo <bala@amypo.in>
2025-12-15 16:11:20 +0000

    User commit

--- src/main/java/com/example/demo/model/ApplicationProfile.java
+++ src/main/java/com/example/demo/model/ApplicationProfile.java
@@ -1,8 +1,18 @@
 package com.example.demo.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class ApplicationProfile {
     private long id;
     private User user;
     private String fullName;
-    private LoaclDate dateOfBirth;
+    private LocalDate dateOfBirth;
+    private String employmentType;
+    private BigDecimal monthlyIncome;
+    private BigDecimal existingEmiObligations;
+    private Integer creditScore;
+    private Integer internalRiskScore;
+    private String country;
+    
 }
