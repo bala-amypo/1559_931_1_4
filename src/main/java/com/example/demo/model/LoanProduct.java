a2fc20fdf0d3908022e419c3a4d25a1f1580bccd
bala-amypo <bala@amypo.in>
2025-12-15 15:56:21 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/LoanProduct.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+
+public class LoanProduct {
+    private long id;
+    private String productCode;
+    private String productName;
+    private String loanType;
+    private BigDecimal minAmount;
+    prvate Big
+}
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -8,6 +8,45 @@ public class User {
 
     public User(){}
 
+    public User(long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     
 
 }
