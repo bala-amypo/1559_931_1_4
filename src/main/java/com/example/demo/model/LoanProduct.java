4d453633ad67de26f6a24c7c2b23621949cd0091
bala-amypo <bala@amypo.in>
2025-12-15 16:01:20 +0000

    User commit

--- src/main/java/com/example/demo/model/LoanProduct.java
+++ src/main/java/com/example/demo/model/LoanProduct.java
@@ -8,5 +8,79 @@ public class LoanProduct {
     private String productName;
     private String loanType;
     private BigDecimal minAmount;
-    prvate Big
+    private BigDecimal maxAmount;
+    private int minTenureMonths;
+    private int maxTenureMonths;
+    private double annualInterestRatePercent;
+    private int baseRiskWeight;
+
+
+    public LoanProduct(){}
+
+
+    public LoanProduct(String productCode, String productName, String loanType, BigDecimal minAmount,
+            BigDecimal maxAmount, int minTenureMonths, int maxTenureMonths, double annualInterestRatePercent,
+            int baseRiskWeight) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.loanType = loanType;
+        this.minAmount = minAmount;
+        this.maxAmount = maxAmount;
+        this.minTenureMonths = minTenureMonths;
+        this.maxTenureMonths = maxTenureMonths;
+        this.annualInterestRatePercent = annualInterestRatePercent;
+        this.baseRiskWeight = baseRiskWeight;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+
+    public void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+
+
+    public void setMinAmount(BigDecimal minAmount) {
+        this.minAmount = minAmount;
+    }
+
+
+    public void setMaxAmount(BigDecimal maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+
+
+    public void setMinTenureMonths(int minTenureMonths) {
+        this.minTenureMonths = minTenureMonths;
+    }
+
+
+    public void setMaxTenureMonths(int maxTenureMonths) {
+        this.maxTenureMonths = maxTenureMonths;
+    }
+
+
+    public void setAnnualInterestRatePercent(double annualInterestRatePercent) {
+        this.annualInterestRatePercent = annualInterestRatePercent;
+    }
+
+
+    public void setBaseRiskWeight(int baseRiskWeight) {
+        this.baseRiskWeight = baseRiskWeight;
+    }
+
+    
 }
