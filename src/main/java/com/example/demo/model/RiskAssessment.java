22bfd212590d82d8d90c86533faecaddb2392bd5
bala-amypo <bala@amypo.in>
2025-12-15 16:31:20 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskAssessment.java
+++ src/main/java/com/example/demo/model/RiskAssessment.java
@@ -1,6 +1,17 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
 public class RiskAssessment {
     private long id;
-    private LoanApplication loan
+    private LoanApplication loanApplication;
+    private String eligibilityStatus;
+    private String riskLevel;
+    private int score;
+    private double debtToIncomeRatio;
+    private String remarks;
+    private LocalDateTime evaluatedOn;
+
+
+    public Risk
 }
