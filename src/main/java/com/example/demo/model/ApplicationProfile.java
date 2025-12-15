fc9eceb68690606758636355b68d8282cc068b06
bala-amypo <bala@amypo.in>
2025-12-15 16:16:20 +0000

    User commit

--- src/main/java/com/example/demo/model/ApplicationProfile.java
+++ src/main/java/com/example/demo/model/ApplicationProfile.java
@@ -14,5 +14,112 @@ public class ApplicationProfile {
     private Integer creditScore;
     private Integer internalRiskScore;
     private String country;
+    private  boolean active;
+
+    public ApplicationProfile() {}
+
+    public ApplicationProfile(User user, String fullName, LocalDate dateOfBirth, String employmentType,
+            BigDecimal monthlyIncome, BigDecimal existingEmiObligations, Integer creditScore, Integer internalRiskScore,
+            String country, boolean active) {
+        this.user = user;
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+        this.employmentType = employmentType;
+        this.monthlyIncome = monthlyIncome;
+        this.existingEmiObligations = existingEmiObligations;
+        this.creditScore = creditScore;
+        this.internalRiskScore = internalRiskScore;
+        this.country = country;
+        this.active = active;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setEmploymentType(String employmentType) {
+        this.employmentType = employmentType;
+    }
+
+    public void setMonthlyIncome(BigDecimal monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public void setExistingEmiObligations(BigDecimal existingEmiObligations) {
+        this.existingEmiObligations = existingEmiObligations;
+    }
+
+    public void setCreditScore(Integer creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public void setInternalRiskScore(Integer internalRiskScore) {
+        this.internalRiskScore = internalRiskScore;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getEmploymentType() {
+        return employmentType;
+    }
+
+    public BigDecimal getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public BigDecimal getExistingEmiObligations() {
+        return existingEmiObligations;
+    }
+
+    public Integer getCreditScore() {
+        return creditScore;
+    }
+
+    public Integer getInternalRiskScore() {
+        return internalRiskScore;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
     
 }
---
+++ src/main/java/com/example/demo/model/LoanApplication.java
@@ -0,0 +1,6 @@
+package com.example.demo.model;
+
+public class LoanApplication {
+    private long id;
+    private ApplicantProfile
+}
